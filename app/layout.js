6779dfe3505dbfc6af7e8c55331e3b69dbf05113
rirefat <rirefat.official@gmail.com>
2024-08-01 21:03:38 +0600

    read data from doc files

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { getDocuments } from "@/lib/doc";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const allDocuments = getDocuments();
+  
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
