27bebf7c16de1d4585cdf1c8966d00b7c4a6129a
rirefat <rirefat.official@gmail.com>
2024-08-04 03:10:08 +0600

    done basic scheleton of main layout

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <footer className="mx-auto max-w-2xl space-y-10 pb-16 lg:max-w-5xl">
+            <div className="relative h-8"></div>
+            <div
+                className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
+                <p className="text-xs text-zinc-600 dark:text-zinc-400">
+                    © Copyright 2024. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
