6bf5930c0d774c6ff90a690aa291fd86f2115b88
rirefat <rirefat.official@gmail.com>
2024-08-12 10:44:14 +0600

    update media files

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <div className="hidden lg:flex">
-            <Link aria-label="Home" href="/">
+            <Link aria-label="Home" href="/" className="flex gap-1">
                 <Image
-                    src="/logo.svg"
+                    src="/logo.png"
                     alt="DevScript"
                     className="h-6 w-auto"
-                    width={100}
-                    height={24}
+                    width={250}
+                    height={250}
                     priority
                 />
+                <span className="font-bold">DevScript</span>
             </Link>
         </div>
     );
