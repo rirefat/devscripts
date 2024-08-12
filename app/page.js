79c4df6592fe7f2155ecbe378ec6cdf5ab3cc535
rirefat <rirefat.official@gmail.com>
2024-08-12 07:59:57 +0600

    update main page section

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="flex flex-col lg:pb-6 lg:col-span-2 justify-center">
               <h1
                 className="mt-4 text-6xl font-extrabold leading-none tracking-tight text-slate-900 sm:text-5xl sm:leading-[3.5rem]">
-                Protocol
+                DevScripts
               </h1>
 
               <p className="mt-6 text-base leading-7 text-slate-700">
@@ -22,10 +22,11 @@ const Home = () => {
               </p>
 
               <div className="mt-10 flex flex-wrap gap-4">
-                <Link href="/"
+                <Link href="/docs/introduction"
                   className="inline-flex justify-center rounded-lg bg-slate-900 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700"
-                  target="_blank">
-                  <span>Get Started</span></Link>
+                >
+                  <span>Get Started</span>
+                </Link>
               </div>
             </div>
             <div className="relative lg:col-span-3">
