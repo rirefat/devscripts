a5f0415af246f7df8bd493c7b75f871cb370c675
rirefat <rirefat.official@gmail.com>
2024-08-12 07:01:20 +0600

    filter displayed content based on categories

--- app/categories/[name]/page.js
+++ app/categories/[name]/page.js
@@ -1,7 +1,14 @@
+import ContentDisplay from "@/components/ContentDisplay";
+import { getDocuments } from "@/lib/doc";
+import { docFilterByCategory } from "@/lib/docFilter";
+
 export default function CategoriesPage({ params: { name } }) {
+    const docs = getDocuments();
+    const matchedDocs = docFilterByCategory(docs, name);
+
     return (
         <div>
-            {name}
+            <ContentDisplay docId={matchedDocs[0].id} />
         </div>
     );
 }
