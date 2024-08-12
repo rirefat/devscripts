18002644c0874c419754d6ab496c220332f3eb69
rirefat <rirefat.official@gmail.com>
2024-08-12 15:26:08 +0600

    adding active link feature

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-
 export default function Sidebar({ docs }) {
     const pathName = usePathname();
     const [rootNodes, setRootNodes] = useState([]);
@@ -27,7 +26,6 @@ export default function Sidebar({ docs }) {
             matchedDocs = docFilterByCategory(docs, category);
 
         }
-
 
         const roots = matchedDocs.filter(doc => !doc.parent);
         const nonRoots = groupBy(
@@ -58,7 +56,7 @@ export default function Sidebar({ docs }) {
 
     }, [pathName, docs])
 
-
+    const isActive = (href) => pathName === href;
 
     return (
         <nav className="lg:block my-10">
@@ -66,13 +64,15 @@ export default function Sidebar({ docs }) {
                 <div className="relative mt-3 pl-2">
                     <div className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"></div>
                     <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
-                    <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
                     <ul role="list" className="border-l border-transparent">
                         {rootNodes.map((rootNode) => (
                             <li key={rootNode.id} className="relative">
+                                {isActive(`/docs/${rootNode.id}`) && (
+                                    <div className="absolute left-0 h-6 w-px bg-emerald-500"></div>
+                                )}
                                 <Link
                                     aria-current="page"
-                                    className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
+                                    className={`flex justify-between gap-2 py-1 pl-4 pr-3 text-sm transition ${isActive(`/docs/${rootNode.id}`) ? 'text-emerald-500 font-bold' : 'text-zinc-900 dark:text-white'}`}
                                     href={`/docs/${rootNode.id}`}
                                 >
                                     <span className="truncate">
@@ -86,9 +86,12 @@ export default function Sidebar({ docs }) {
                                     >
                                         {nonRootNodesGrouped[rootNode.id].map(
                                             (subRoot) => (
-                                                <li key={subRoot.id}>
+                                                <li key={subRoot.id} className="relative">
+                                                    {isActive(`/docs/${rootNode.id}/${subRoot.id}`) && (
+                                                        <div className="absolute left-0 h-6 w-px bg-emerald-500"></div>
+                                                    )}
                                                     <Link
-                                                        className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+                                                        className={`flex justify-between gap-2 py-1 pl-7 pr-3 text-sm transition ${isActive(`/docs/${rootNode.id}/${subRoot.id}`) ? 'text-emerald-500 font-bold' : 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white'}`}
                                                         href={`/docs/${rootNode.id}/${subRoot.id}`}
                                                     >
                                                         <span className="truncate">
