cff211b944a44eea041af5cdbbc7e7d773bab371
rirefat <rirefat.official@gmail.com>
2024-08-13 21:56:27 +0600

    make content display component responsive

--- components/ContentDisplay.jsx
+++ components/ContentDisplay.jsx
@@ -4,45 +4,49 @@ import Tag from "./Tag";
 import Feedback from "./Feedback";
 import { Toaster } from "sonner";
 
-
 const ContentDisplay = async ({ docId }) => {
     const documentContent = await getDocumentContent(docId);
 
     return (
-        <div className="">
-            <article class="prose dark:prose-invert mb-24">
-                <h1>{documentContent.title}</h1>
+        <div className="px-4 md:px-8 lg:px-16">
+            <article className="prose dark:prose-invert mb-8 lg:mb-24">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">{documentContent.title}</h1>
 
-                <div>
-                    {
-                        documentContent.date && <span>{`Published on ${documentContent.date} by `}</span>
-                    }
-                    {
-                        documentContent.author && <><Link href={`/authors/${documentContent.author}`}>{`${documentContent.author} `}</Link> <span>under the </span></>
-                    }
-                    {
-                        documentContent.category && <><Link href={`/categories/${documentContent.category}`}>{`${documentContent.category} `}</Link><span>category.</span></>
-                    }
+                <div className="text-sm md:text-base mt-2">
+                    {documentContent.date && <span>{`Published on ${documentContent.date} by `}</span>}
+                    {documentContent.author && (
+                        <>
+                            <Link href={`/authors/${documentContent.author}`} className="text-blue-600 hover:underline">
+                                {`${documentContent.author} `}
+                            </Link>{" "}
+                            <span>under the </span>
+                        </>
+                    )}
+                    {documentContent.category && (
+                        <>
+                            <Link href={`/categories/${documentContent.category}`} className="text-blue-600 hover:underline">
+                                {`${documentContent.category} `}
+                            </Link>
+                            <span>category.</span>
+                        </>
+                    )}
                 </div>
 
-                <div>
-                    {
-                        documentContent.tags &&
-                        documentContent.tags.map((tag) => <Tag key={tag} tag={tag} />)
-                    }
+                <div className="mt-4">
+                    {documentContent.tags &&
+                        documentContent.tags.map((tag) => <Tag key={tag} tag={tag} />)}
                 </div>
 
                 <div
-                    className="lead"
-                    dangerouslySetInnerHTML={{ __html: documentContent.contentHTML }} />
-
+                    className="lead mt-4"
+                    dangerouslySetInnerHTML={{ __html: documentContent.contentHTML }}
+                />
             </article>
 
             {/* Feedback from users */}
             <Feedback />
             <Toaster />
         </div>
-
     );
 };
 
